1e266300498ab6d6117e5623e72aa0d1904d4ca8
davydka <davydka@gmail.com>
2019-06-22 10:38:44 -0400

    Change shopping cart cta from non-actionable href to button

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import shoppingCartIcon from './shoppingCartIcon'
 
-const ShoppingCart = props => (
-  <div className='shopping-cart'>
-    <a href='#'>
-      {shoppingCartIcon()} Your cart is empty
-    </a>
-  </div>
-)
+const ShoppingCart = props => {
+  return (
+    <div className='shopping-cart'>
+      <button className='reset-button' onClick={props.handleCartClicked}>
+        {shoppingCartIcon()} Your cart is empty
+      </button>
+    </div>
+  )
+}
 
 export default ShoppingCart
