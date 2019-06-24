16a59069770c2cc1a7e9bfc975be0eeed4b32da7
davydka <davydka@gmail.com>
2019-06-24 00:43:59 -0400

    Adapting to new api structure

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,13 +13,17 @@ const getAddedIds = state => fromCart.getAddedIds(state.cart)
 const getQuantity = (state, id) => fromCart.getQuantity(state.cart, id)
 const getProduct = (state, id) => fromProducts.getProduct(state.products, id)
 
-export const getTotal = state =>
-  getAddedIds(state)
+export const getTotal = state => {
+  console.log(state)
+  return (
+    getAddedIds(state)
     .reduce((total, id) =>
-      total + getProduct(state, id).price * getQuantity(state, id),
+      total + getProduct(state, id).price.value * getQuantity(state, id),
       0
     )
     .toFixed(2)
+  )
+}
 
 export const getCartProducts = state =>
   getAddedIds(state).map(id => ({
