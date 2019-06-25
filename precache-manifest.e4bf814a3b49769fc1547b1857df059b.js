b3b6eab212ea6332e83972490105ccc673f9c75c
davydka <davydka@gmail.com>
2019-06-24 22:05:50 -0400

    Updates

---
+++ precache-manifest.e4bf814a3b49769fc1547b1857df059b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "48cc64a34d038c16c679390160aedaa1",
+    "url": "/cart-assessment/static/media/shoppingCartClose.48cc64a3.svg"
+  },
+  {
+    "revision": "8f91e421457c2820541a",
+    "url": "/cart-assessment/static/js/runtime~main.8f91e421.js"
+  },
+  {
+    "revision": "22c255ce76c778dc34a3",
+    "url": "/cart-assessment/static/js/main.22c255ce.chunk.js"
+  },
+  {
+    "revision": "cf33299d3f6617b29590",
+    "url": "/cart-assessment/static/js/2.cf33299d.chunk.js"
+  },
+  {
+    "revision": "22c255ce76c778dc34a3",
+    "url": "/cart-assessment/static/css/main.8212d3a5.chunk.css"
+  },
+  {
+    "revision": "e0cbbfea3d314a83c6733b196e611f28",
+    "url": "/cart-assessment/index.html"
+  }
+];
--- tasks.md
+++
@@ -1,5 +0,0 @@
-## Tasks
-
-1. [Implement Responsive Design](/tasks/01-responsive-design.md)
-2. [Enhance Cart Functionality](/tasks/02-cart-enhancements.md)
-3. [Hook Up Product API](/tasks/03-product-api.md)
